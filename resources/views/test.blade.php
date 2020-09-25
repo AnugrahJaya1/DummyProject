b3617cc0055d13f600b3f0687b625f32418243f7
unknown <anugrahjaya23@gmail.com>
2020-09-25 11:39:24 +0700

    test blade

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,10 +1,3 @@
-<?php
-
-use App\Http\Controllers\FormulirController;
-
-$name = ['','a','b','c','d','e'];
-    $count = 0;
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +8,33 @@ $name = ['','a','b','c','d','e'];
 </head>
 
 <body>
-    {{print_r($name}}
+    {{$name}}
+    <ol>
+        <!-- 1 -->
+        <li>Sholat 5 waktu di rumah</li>
+
+        <?php
+        $arr = ['','Sangat Kurang', 'Kurang', 'Cukup', 'Baik', 'Sangat Baik'];
+            for($i = 1;$i<=5;$i++){
+                echo "<input type='radio' id='".$name.$i."' name='".$name."' value='".$i."'>";
+                echo "<label for='".$name.$i."'>".$arr[$i]."</label>";
+            }
+        ?>
+        <input type="radio" id="a11" name={{$name}} value="1">
+        <label for='a11'>Sangat Kurang</label>
+
+        <input type="radio" id="a12" name="a1" value="2">
+        <label for='a12'>Kurang</label>
+
+        <input type="radio" id="a13" name="a1" value="3">
+        <label for='a13'>Cukup</label>
+
+        <input type="radio" id="a14" name="a1" value="4">
+        <label for='a14'>Baik</label>
+
+        <input type="radio" id="a15" name="a1" value="5">
+        <label for='a15'>Sangat Baik</label>
+    </ol>
 </body>
 
 </html>
